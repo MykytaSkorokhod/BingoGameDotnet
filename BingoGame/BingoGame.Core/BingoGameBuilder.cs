25e939602ea602715843fdd8a3c28ec6e8218c38
MykytaSkorokhod <nikita.skorokhod@wtt-solutions.com>
2023-01-29 00:34:04 +0200

    init commit

---
+++ BingoGame/BingoGame.Core/BingoGameBuilder.cs
@@ -0,0 +1,31 @@
+﻿using BingoGame.Core.Instance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingoGame.Core
+{
+    public class BingoGameBuilder
+    {
+        BingoGameInstance? currentInstance;
+
+        public BingoGameBuilder()
+        {
+            currentInstance = new BingoGameInstance();
+        }
+
+        public IBingoGameInstance Build()
+        {
+            try
+            {
+                return currentInstance;
+            }
+            finally
+            {
+                currentInstance = null;
+            }
+        }
+    }
+}
